1825da7d53dc01e386e11a01e8dc8460d76e3ae1
hamza nachid <hamzanachid82@gmail.com>
2023-09-01 15:20:41 +0100

    create an extension for open whatsapp youtube and twitter with alt

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.commands.onCommand.addListener(function (command) {
+    if(command==="open-whatsapp"){
+        chrome.tabs.create({url:"https://web.whatsapp.com/"})
+    }
+    else if(command==="open-youtube"){
+        chrome.tabs.create({url:"https://www.youtube.com/"})
+    }
+    else if(command==="open-twitter"){
+        chrome.tabs.create({url:"https://www.twitter.com/"})
+    }
+})
